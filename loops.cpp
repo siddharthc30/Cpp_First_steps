33933cbbd8196943f8c59275daccf479dca8b3fb
Siddharth <siddharthcilamkoti@gmail.com>
2022-02-15 12:32:38 +0530

    loops

---
+++ loops.cpp
@@ -0,0 +1,32 @@
+//while loop 
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int i = 1;
+    while(i <= 5)
+    {
+        cout << i << endl;
+        i++;
+    }
+}
+
+//do while loop
+/* do {
+    statements
+}
+while(condition)
+*/
+
+// for loop
+
+/* for (int i =0; i<10; i++)
+{
+    cout<< i <<end;
+}
+*/
+
+
+
